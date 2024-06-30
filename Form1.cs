b65972c3024561f6a25ec667e3925aaaa32ff865
chistokrovnij <fsknsocco@gmail.com>
2024-06-30 21:38:24 +0300

    first commit

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        private int passwordlength; 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = PasswordGenerator.GenerateRandomPassword(passwordlength);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrEmpty(textBox1.Text))
+            {
+                Clipboard.SetText(textBox1.Text);
+                MessageBox.Show("Password copied", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            } else
+            {
+                MessageBox.Show("First, generate a password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox2.Text, out int length))
+            {
+                passwordlength = length;
+            }
+        }
+    }
+}
---
+++ PasswordGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp7
+{
+    public class PasswordGenerator
+    {
+        public static string GenerateRandomPassword(int length)
+        {
+            const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*";
+            StringBuilder result = new StringBuilder();
+            Random random = new Random();
+            for(int i = 0; i < length; i++)
+            {
+                result.Append(validChars[random.Next(validChars.Length)]);
+            }
+            return result.ToString();
+        }
+    }
+}
